0df8f47dc9940de55ed3dfe2042dd7998cf4fbfd
anderav1 <avabc5@umsystem.edu>
2022-04-18 11:55:16 -0500

    Copied files from proj2

---
+++ main.cpp
@@ -0,0 +1,49 @@
+// Lexi Anderson
+// CS4280, Proj 3
+// main.cpp
+
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+
+#include "node.h"
+#include "parser.h"
+#include "testTree.h"
+
+std::string fileName;
+
+void printArgV(int, char*[]);
+
+
+int main(int argc, char* argv[]) {
+  std::istream* fp;
+  
+  // TODO: implement static semantics
+  
+  if (argc == 0 || argc > 2) {
+    printf("Fatal: Improper usage\nUsage:");
+    printArgV(argc, argv);
+    exit(1);
+  }
+  else if (argc == 2) fp = new std::ifstream(argv[1]);
+  else if (argc == 1) fp = &std::cin;
+  
+  node_t* root = parser(*fp);
+  printTree(root, 0);
+  
+  if (fp != &std::cin) delete fp;
+  deleteTree(root);
+  
+  std::cout << "Finished parsing" << std::endl;
+  
+  return 0;
+}
+
+// Print the entered command followed by line break
+// argc -- argument count
+// argv -- argument array
+void printArgV(int argc, char* argv[]) {
+  int i;
+  for (i = 0; i < argc; i++) std::cout << " " << argv[i];
+  std::cout << std::endl;
+}
